991b12ee27f711ae7e09ffce6fe2b0ec83264236
Hetarth Thakor <GD04Hetarth@thecea.ca>
2022-07-10 15:36:44 -0700

    Added sfx with audio reverb

--- Scripts/Scripts from youtube/Sabers.cs
+++ Scripts/Scripts from youtube/Sabers.cs
@@ -10,6 +10,12 @@ public class Sabers : MonoBehaviour
     public LayerMask layer;  // select color via layer
     private Vector3 previousPos;
     public string level;
+    public AudioSource _break;
+
+    private void Start()
+    {
+        _break = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,8 +25,9 @@ public class Sabers : MonoBehaviour
         {
             if(Vector3.Angle(transform.position-previousPos,hit.transform.up)>130)
             {
-                ScoreManager.score++;
-                Destroy(hit.transform.gameObject); // add score and destroy the cube for FPS!
+                _break.Play();
+                ScoreManager.score++;              
+                Destroy(hit.transform.gameObject, 1.5f); // add score and destroy the cube for FPS!
             }
         }
         previousPos = transform.position;
